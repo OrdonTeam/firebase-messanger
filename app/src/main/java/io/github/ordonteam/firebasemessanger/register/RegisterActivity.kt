bde49e45cc2151168d2d406fc62c7a7a7592f3df
Karol Kowalski <ordon1313@gmail.com>
2016-09-24 21:21:35 +0200

    Configure kotlin with empty activity

---
+++ app/src/main/java/io/github/ordonteam/firebasemessanger/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package io.github.ordonteam.firebasemessanger.register
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.github.ordonteam.firebasemessanger.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_activity)
+    }
+}
